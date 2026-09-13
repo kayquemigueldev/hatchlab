be5d61aa042c108bf5dff8ad6621c4674e054ea2
kayquemigueldev <kayquemiguel.dev@gmail.com>
2026-09-13 15:48:25 -0300

    feat: initialize Spring Boot backend

---
+++ backend/src/main/java/com/hatchlab/HatchlabBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hatchlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HatchlabBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HatchlabBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hatchlab/HatchlabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hatchlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HatchlabBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
