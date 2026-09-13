06d00ee5f48b126175ed6f9b080f526d6e303c78
kayquemigueldev <kayquemiguel.dev@gmail.com>
2026-09-13 16:13:26 -0300

    fix: disable default Spring Security user

--- backend/src/main/java/com/hatchlab/HatchlabBackendApplication.java
+++ backend/src/main/java/com/hatchlab/HatchlabBackendApplication.java
@@ -2,12 +2,12 @@ package com.hatchlab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class HatchlabBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HatchlabBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HatchlabBackendApplication.class, args);
+    }
 }
